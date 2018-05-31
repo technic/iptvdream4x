130aafe7c2bb8c2ff4c54d7ad8fb7da0646a863c
Alex Maystrenko <alexeytech@gmail.com>
2018-05-31 14:15:52 +0200

    fix docker-run.sh - specify image tag

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # run test script in docker
-docker run -it -v `pwd`:/work technic93/e2xvfb sudo ./update-test.py
+docker run -it -v `pwd`:/work technic93/e2xvfb:0.1 python update-test.py
