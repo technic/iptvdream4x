c28f1af9515082d40509fcb0450dc875cffe70e0
Alex Maystrenko <alexeytech@gmail.com>
2018-12-28 15:14:28 +0200

    Add pure tv

---
+++ src/api/pure.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#  enigma2 iptv player
+#
+#  Copyright (c) 2010 Alex Maystrenko <alexeytech@gmail.com>
+#
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2, or (at your option) any later
+# version.
+
+from __future__ import print_function
+
+from api1 import TeleportStream
+
+
+# This is Raduga clone with own branding
+
+class OTTProvider(TeleportStream):
+	NAME = "PureTV"
+	site = "http://core.raduga.tv/iptv/api/v1/json"
