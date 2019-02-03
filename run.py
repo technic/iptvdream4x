42ca1fa8617274cfe74631eca161dd22dbf8ccf1
Alex Maystrenko <alexeytech@gmail.com>
2019-02-03 18:59:34 +0100

    Add vm-docker image that has gstreamer and skins

--- run.py
+++ run.py
@@ -2,4 +2,7 @@ from subprocess import check_call
 
 if __name__ == "__main__":
     # You should have Xming running on your host machine
-    check_call(["docker", "exec", "-i", "-e", "DISPLAY=host.docker.internal:0.0", "enigma2_box", "enigma2"])
+    check_call(["docker", "exec", "-i",
+                "-e", "DISPLAY=:99",
+                "-e", "ENIGMA_DEBUG_LVL=5",
+                "enigma2_box", "sudo", "-E", "enigma2"])
