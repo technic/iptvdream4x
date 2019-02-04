88b1ae395a7aa0c3bf87e51c6e9139bd592b71dd
Alex Maystrenko <alexeytech@gmail.com>
2019-02-04 18:03:34 +0100

    run.py - kill enigma2 before starting new instance

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from subprocess import check_call
+from subprocess import call, check_call
 
 if __name__ == "__main__":
-    # You should have Xming running on your host machine
-    check_call(["docker", "exec", "-i",
+    call(["docker", "exec", "enigma2_box", "sudo", "killall", "enigma2"])
+    check_call(["docker", "exec",
                 "-e", "DISPLAY=:99",
                 "-e", "ENIGMA_DEBUG_LVL=5",
                 "enigma2_box", "sudo", "-E", "enigma2"])
