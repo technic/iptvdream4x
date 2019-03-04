52456b3d009c4143e4f0929b0ba1bf87a9290b96
Alex Maystrenko <alexeytech@gmail.com>
2019-03-04 12:17:45 +0100

    allow to set Xvfb resolution

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ sudo mkdir -p /dev/input
 sudo service apache2 start
 
 echo "start Xvfb"
-Xvfb :99 -ac -screen 0 1280x720x16 &
+test -z "$RESOLUTION" && RESOLUTION="1280x720x16"
+Xvfb :99 -ac -screen 0 "$RESOLUTION" &
 xvfb_pid=$!
 export DISPLAY=:99
 echo "exec command $@"
