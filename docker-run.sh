db428e3ffd147a628237940d9b818b11d04f4e37
Alex Maystrenko <alexeytech@gmail.com>
2019-03-06 23:10:55 +0100

    use e2xvfb:latest tag

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # run test script in docker
-docker run -it -v "$(pwd)":/work -e DESTDIR=/tmp/build technic93/e2xvfb:0.1 sudo -E python update-test.py
+docker run -it -v "$(pwd)":/work -e DESTDIR=/tmp/build technic93/e2xvfb sudo -E python update-test.py
