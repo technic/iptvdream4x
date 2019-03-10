22f484ab8f70e3708670f00bcc86de471ce566ac
Alex Maystrenko <alexeytech@gmail.com>
2019-03-10 23:38:59 +0100

    Add license header

--- tests/test_ktvStream.py
+++ tests/test_ktvStream.py
@@ -1,4 +1,15 @@
+# -*- coding: utf-8 -*-
+#  enigma2 iptv player
+#
+#  Copyright (c) 2018 Alex Maystrenko <alexeytech@gmail.com>
+#
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2, or (at your option) any later
+# version.
+
 from __future__ import print_function
+
 from unittest import TestCase
 from src.api.kartina import OTTProvider
 from src.utils import Group, Channel, EPG
