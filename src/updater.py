2759817f5352f12b25f5dbefae1f546cc52c0e79
Alex Maystrenko <alexeytech@gmail.com>
2019-07-30 20:01:35 +0200

    Add ftp deploy and change update url

--- src/updater.py
+++ src/updater.py
@@ -69,7 +69,7 @@ def parseVersion(data):
 
 class Updater(object):
 	def __init__(self):
-		self.url = "http://technic.cf/iptvdream4x/packages/"
+		self.url = "http://cb.technic.cf/iptvdream4x/packages/"
 		self.console = Console()
 		self.prefix = "enigma2-plugin-extensions"
 		self.agent = "IPtvDream-%s/%s" % (NAME, VERSION)
