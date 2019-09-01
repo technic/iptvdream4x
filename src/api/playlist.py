e52f65dfd80a097093804d3f149d3ac0d2ef3596
Alex Maystrenko <alexeytech@gmail.com>
2019-09-01 16:53:42 +0200

    Add playlist file WIP

---
+++ src/api/playlist.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#  enigma2 iptv player
+#
+#  Copyright (c) 2018 Alex Maystrenko <alexeytech@gmail.com>
+#
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2, or (at your option) any later
+# version.
+
+from __future__ import print_function
+
+# plugin imports
+from m3u import M3UProvider
+from ..utils import APIException
+
+
+class OTTProvider(M3UProvider):
+	NAME = "M3U-Playlist"
+
+	def __init__(self, username, password):
+		super(OTTProvider, self).__init__(username, password)
+		self.site = "http://iptvdream.zapto.org/epg-soveni"
+		self.playlist = "playlist.m3u"
+
+	def start(self):
+		pass
+
+	def setChannelsList(self):
+		m3u = self._locatePlaylist()
+		try:
+			self._parsePlaylist(m3u)
+		except IOError as e:
+			self.trace("error!", e)
+			raise APIException(e)
