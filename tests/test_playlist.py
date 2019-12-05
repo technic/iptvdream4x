221ad9cffe424cfaf40a17c4df95a58f43007c54
Alex Maystrenko <alexeytech@gmail.com>
2019-12-05 19:03:33 +0100

    Add some benchmark code for m3u parsing

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -15,9 +15,27 @@ from src.api.playlist import OTTProvider
 
 
 class Test(ott_provider.TestOTTProvider):
-    ProviderClass = OTTProvider
+	ProviderClass = OTTProvider
+
+	def bench_m3u_parser(self):
+		prov = OTTProvider("", "")
+		prov._downloadTvgMap()
+
+		def parse():
+			m3u = prov._locatePlaylist()
+			with open(m3u) as f:
+				prov._parsePlaylist(f.readlines())
+
+		import timeit
+		from math import sqrt
+		K = 3
+		N = 30
+		t = timeit.repeat(parse, number=N, repeat=K)
+		ta = sum(t) / K
+		mse = sqrt(sum((t1 - ta)**2 for t1 in t) / K)
+		print("TIME %f ± %f per loop" % (max(t) / N, mse / N))
 
 
 if __name__ == "__main__":
-    from unittest import main
-    main()
+	from unittest import main
+	main()
