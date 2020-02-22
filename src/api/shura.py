c2a42ecc56e4c4089ddff074c121b084c123c1a3
Alex Maystrenko <alexeytech@gmail.com>
2020-02-22 10:32:41 +0100

    shura-tv use official playlist url

--- src/api/shura.py
+++ src/api/shura.py
@@ -12,33 +12,73 @@ from __future__ import print_function
 
 # system imports
 import re
+from urllib2 import HTTPError
+from json import loads as json_loads
 
 # plugin imports
 from m3u import M3UProvider
-from ..utils import Channel
+from abstract_api import JsonSettings
+from ..utils import Channel, APIException, APILoginFailed, ConfInteger
+try:
+	from ..loc import translate as _
+except ImportError:
+	def _(text):
+		return text
 
 
-class OTTProvider(M3UProvider):
+class OTTProvider(JsonSettings, M3UProvider):
 	NAME = "ShuraTV"
-	TVG_MAP = True
+	AUTH_TYPE = "Login"
+	TVG_MAP = False
 
 	def __init__(self, username, password):
 		super(OTTProvider, self).__init__(username, password)
-		self.site = "http://technic.cf/epg-soveni/"
-		self.playlist = "shura_pl.m3u8"
-		self.playlist_url = "http://soveni.leolitz.info/plist/shura_epg_ico.m3u8"
+		self.site = "http://technic.cf/epg-1ott/"
+		server = self.getSettings()['server'].value
+		self.playlist_url = "http://pl.tvshka.net/?uid=%s&srv=%s&type=halva" % (username, server)
 		self._url_regexp = re.compile(r"https?://[\w.]+/~\w+/(\d+)/hls/.*\.m3u8")
+		self.name_map = {}
 
 	def start(self):
-		url_regexp = re.compile(r"https?://([\w.]+)/(\w+)/\d+/hls/pl.m3u8")
-		self._extractKeyFromPlaylist(url_regexp)
+		try:
+			self.name_map = json_loads(self.readHttp(self.site + "/channels_names"))['data']
+		except IOError as e:
+			self.trace("error!", e)
+			raise APIException(e)
+
+		self._downloadTvgMap()
+		try:
+			self._parsePlaylist(self.readHttp(self.playlist_url).split('\n'))
+		except HTTPError as e:
+			self.trace("HTTPError:", e, type(e), e.getcode())
+			if e.code in (403, 404):
+				raise APILoginFailed(e)
+			else:
+				raise APIException(e)
+		except IOError as e:
+			self.trace("IOError:", e, type(e))
+			raise APIException(e)
+
+	def setChannelsList(self):
+		# Channels are downloaded during start, to allow handling login exceptions
+		pass
 
 	def makeChannel(self, num, name, url, tvg, logo, rec):
+		if tvg is None:
+			try:
+				tvg = self.name_map[name.decode('utf-8')]
+			except KeyError:
+				pass
 		m = self._url_regexp.match(url)
 		if m:
 			cid = int(m.group(1))
 		else:
 			cid = hash(url)
 			self.trace("Failed to get cid from url", url)
-		url = url.replace("localhost", self._domain).replace("00000000000000", self._key)
 		return Channel(cid, name, num, rec), {'tvg': tvg, 'url': url, 'logo': logo}
+
+	def getSettings(self):
+		settings = {
+			'server': ConfInteger(_("Server"), 1, (0, 10000)),
+		}
+		return self._safeLoadSettings(settings)
