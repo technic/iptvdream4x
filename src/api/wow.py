3392d339f51322f1cd995f0aee1c615419b18f1c
Alex Maystrenko <alexeytech@gmail.com>
2020-05-30 23:48:11 +0200

    update wow tv epg source

--- src/api/wow.py
+++ src/api/wow.py
@@ -10,8 +10,12 @@
 
 from __future__ import print_function
 
+# system imports
+from json import loads as json_loads
+
 # plugin imports
 from m3u import M3UProvider
+from ..utils import Channel, APIException
 
 
 class OTTProvider(M3UProvider):
@@ -20,6 +24,32 @@ class OTTProvider(M3UProvider):
 
 	def __init__(self, username, password):
 		super(OTTProvider, self).__init__(username, password)
-		self.site = "http://iptvdream.zapto.org/epg-soveni/"
+		self.site = "http://iptvdream.zapto.org/epg-it999/"
 		self.playlist = "edem_pl.m3u8"
-		self.playlist_url = "http://soveni.leolitz.info/plist/edem_epg_ico.m3u8"
+		self._tvg_info = {}
+
+	def start(self):
+		try:
+			self._tvg_info = json_loads(self.readHttp('http://technic.cf/iptvdream4x/it999/tvg.json'))
+		except IOError as e:
+			self.trace("error!", e)
+			raise APIException(e)
+
+	def setChannelsList(self):
+		m3u = self._locatePlaylist()
+		with open(m3u) as f:
+			self._parsePlaylist(f.read().split('\n'))
+
+	def makeChannel(self, num, name, url, tvg, logo, rec):
+		m = self._url_regexp.match(url)
+		if m:
+			cid = int(m.group(1))
+		else:
+			cid = hash(url)
+		try:
+			data = self._tvg_info[cid]
+			tvg = data['tvg-id']
+			logo = data['tvg-logo']
+		except KeyError:
+			pass
+		return Channel(cid, name, num, rec), {'tvg': tvg, 'url': url, 'logo': logo}
