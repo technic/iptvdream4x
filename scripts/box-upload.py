4bdcd5e97b1e3a0c0459d0cee80164c548c29075
Alex Maystrenko <alexeytech@gmail.com>
2020-06-07 14:15:26 +0200

    set correct provider in box-upload script

--- scripts/box-upload.py
+++ scripts/box-upload.py
@@ -9,4 +9,4 @@ import json
 if __name__ == "__main__":
 	with open('secret.json') as f:
 		box_ip = json.load(f)['box_ip']
-	check_call(["docker", "exec", "-it", "enigma2", "./box-upload.sh", box_ip])
+	check_call(["docker", "exec", "-it", "enigma2", "./box-upload.sh", box_ip, "WowTV"])
