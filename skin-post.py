bb6585c079e717c2095021cd92bb9776042a836c
Alex Maystrenko <alexeytech@gmail.com>
2020-06-17 22:08:33 +0200

    use panel in skin

--- skin-post.py
+++ skin-post.py
@@ -8,6 +8,43 @@ from __future__ import print_function
 import sys
 from xml.etree.ElementTree import ElementTree, Element
 
+
+def search(element):
+    for attr in element.keys():
+        if attr.lower().find('color') > -1:
+            # print('found color attribute', attr)
+            oldcol = element.get(attr)
+            if oldcol in coldict:
+                element.set(attr, coldict[oldcol])
+                # print('replace', oldcol, 'with', coldict[oldcol])
+            else:
+                # print('maybe good color', oldcol)
+                pass
+    for x in element:
+        search(x)
+
+
+def find_panel(name):
+    for s in root.findall('screen'):
+        if s.get('name') == name:
+            return s
+    raise Exception("Panel not found: %s" % name)
+
+
+used_panels = set()
+
+
+def replace_panel(s):
+    for i, widget in enumerate(s):
+        if widget.tag != 'panel':
+            continue
+        s.remove(widget)
+        name = widget.get('name')
+        used_panels.add(name)
+        for child in find_panel(name):
+            s.insert(i, child)
+
+
 if __name__ == "__main__":
     if not len(sys.argv) > 1:
         print('Specify input and output files!')
@@ -26,23 +63,13 @@ if __name__ == "__main__":
         # print('Add color', colname, colval)
         coldict[colname] = colval
 
-    def search(element):
-        for attr in element.keys():
-            if attr.lower().find('color') > -1:
-                # print('found color attribute', attr)
-                oldcol = element.get(attr)
-                if oldcol in coldict:
-                    element.set(attr, coldict[oldcol])
-                    # print('replace', oldcol, 'with', coldict[oldcol])
-                else:
-                    # print('maybe good color', oldcol)
-                    pass
-        for x in element:
-            search(x)
-
     skin_root = Element('skin')
     for screen in root.findall('screen'):
         search(screen)
-        skin_root.append(screen)
+        replace_panel(screen)
+
+    for screen in root.findall('screen'):
+        if not screen.get('name') in used_panels:
+            skin_root.append(screen)
 
     ElementTree(skin_root).write(sys.argv[2])
