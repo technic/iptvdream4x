e5d1374ed93048412f43bd31ca60947084a1818f
Alex Maystrenko <alexeytech@gmail.com>
2020-10-07 22:34:49 +0200

    73mtv add playlist format option

--- src/api/73mtv.py
+++ src/api/73mtv.py
@@ -13,7 +13,7 @@ from __future__ import print_function
 # plugin imports
 from abstract_api import JsonSettings
 from m3u import M3UProvider
-from ..utils import APILoginFailed
+from ..utils import APILoginFailed, ConfSelection
 try:
 	from ..loc import translate as _
 except ImportError:
@@ -34,10 +34,17 @@ class OTTProvider(JsonSettings, M3UProvider):
 		token = data['token']
 		if not token:
 			raise APILoginFailed(_("Wrong number or pin"))
-		self.playlist_url = 'http://pl.73mtv.net/api/%s/high/ottnav.m3u' % token
+		s = self.getLocalSettings()
+		self.playlist_url = 'http://pl.73mtv.net/api/%s/high/ottnav.%s' % (token, s['format'].value)
 
 	def getStreamUrl(self, cid, pin, time=None):
 		url = self.channels_data[cid]['url']
 		if time:
 			url += '?archive=%s' % time.strftime('%s')
 		return url
+
+	def getLocalSettings(self):
+		settings = {
+			'format': ConfSelection(_("Streaming format"), 'm3u', [('m3u', "TS"), ('m3u8', "HLS")])
+		}
+		return self._safeLoadSettings(settings)
