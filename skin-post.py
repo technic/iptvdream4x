539a4c0f1a43b3fea35b17f6d8a73366229383ad
Alex Maystrenko <alexeytech@gmail.com>
2020-10-25 11:53:55 +0100

    convert indentation to match project settings

--- skin-post.py
+++ skin-post.py
@@ -10,66 +10,66 @@ from xml.etree.ElementTree import ElementTree, Element
 
 
 def search(element):
-    for attr in element.keys():
-        if attr.lower().find('color') > -1:
-            # print('found color attribute', attr)
-            oldcol = element.get(attr)
-            if oldcol in coldict:
-                element.set(attr, coldict[oldcol])
-                # print('replace', oldcol, 'with', coldict[oldcol])
-            else:
-                # print('maybe good color', oldcol)
-                pass
-    for x in element:
-        search(x)
+	for attr in element.keys():
+		if attr.lower().find('color') > -1:
+			# print('found color attribute', attr)
+			oldcol = element.get(attr)
+			if oldcol in coldict:
+				element.set(attr, coldict[oldcol])
+				# print('replace', oldcol, 'with', coldict[oldcol])
+			else:
+				# print('maybe good color', oldcol)
+				pass
+	for x in element:
+		search(x)
 
 
 def find_panel(name):
-    for s in root.findall('screen'):
-        if s.get('name') == name:
-            return s
-    raise Exception("Panel not found: %s" % name)
+	for s in root.findall('screen'):
+		if s.get('name') == name:
+			return s
+	raise Exception("Panel not found: %s" % name)
 
 
 used_panels = set()
 
 
 def replace_panel(s):
-    for i, widget in enumerate(s):
-        if widget.tag != 'panel':
-            continue
-        s.remove(widget)
-        name = widget.get('name')
-        used_panels.add(name)
-        for child in find_panel(name):
-            s.insert(i, child)
+	for i, widget in enumerate(s):
+		if widget.tag != 'panel':
+			continue
+		s.remove(widget)
+		name = widget.get('name')
+		used_panels.add(name)
+		for child in find_panel(name):
+			s.insert(i, child)
 
 
 if __name__ == "__main__":
-    if not len(sys.argv) > 1:
-        print('Specify input and output files!')
-        print('usage: %s <infile> <outfile>' % sys.argv[0])
-        sys.exit(1)
+	if not len(sys.argv) > 1:
+		print('Specify input and output files!')
+		print('usage: %s <infile> <outfile>' % sys.argv[0])
+		sys.exit(1)
 
-    infile = open(sys.argv[1])
-    root = ElementTree()
-    root.parse(infile)
-    colors = root.find('colors')
-    coldict = {}
+	infile = open(sys.argv[1])
+	root = ElementTree()
+	root.parse(infile)
+	colors = root.find('colors')
+	coldict = {}
 
-    for col in colors:
-        colname = col.get('name')
-        colval = col.get('value')
-        # print('Add color', colname, colval)
-        coldict[colname] = colval
+	for col in colors:
+		colname = col.get('name')
+		colval = col.get('value')
+		# print('Add color', colname, colval)
+		coldict[colname] = colval
 
-    skin_root = Element('skin')
-    for screen in root.findall('screen'):
-        search(screen)
-        replace_panel(screen)
+	skin_root = Element('skin')
+	for screen in root.findall('screen'):
+		search(screen)
+		replace_panel(screen)
 
-    for screen in root.findall('screen'):
-        if not screen.get('name') in used_panels:
-            skin_root.append(screen)
+	for screen in root.findall('screen'):
+		if not screen.get('name') in used_panels:
+			skin_root.append(screen)
 
-    ElementTree(skin_root).write(sys.argv[2])
+	ElementTree(skin_root).write(sys.argv[2])
