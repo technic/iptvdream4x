1eea865fdb841302ea6a2a949c93bbb60fdbef90
Alex Maystrenko <alexeytech@gmail.com>
2020-11-13 19:28:29 +0100

    circle-ci: check python3 compatibility

---
+++ scripts/check-py3.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+import sys
+import py_compile
+import glob
+
+
+def check(f):
+	print('check', f)
+	py_compile.compile(f)
+
+
+def main():
+	print("Python", sys.version)
+	files = glob.glob("src/**/*.py")
+	for f in files:
+		check(f)
+
+
+if __name__ == "__main__":
+	main()
