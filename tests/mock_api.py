ef288c2d12ce11ce07697a76d15fa48e64d1c662
Alex Maystrenko <alexeytech@gmail.com>
2021-08-22 18:58:29 +0200

    add hack to fix faling tests on wowtv branch

--- tests/mock_api.py
+++ tests/mock_api.py
@@ -2,6 +2,9 @@ from src.api.edem_soveni import OTTProvider
 
 
 class MockApi(OTTProvider):
+	# hack to fix faling test
+	NAME = "WowTV"
+
 	def start(self):
 		# skip extracting key from playlist
 		pass
